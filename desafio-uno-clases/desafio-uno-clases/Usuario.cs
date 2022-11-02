af1cfb4718ab2cbf2220f5080b1f3e8d8e7e8653
rechegoy <rebeca.echegoy@gmail.com>
2022-11-02 11:42:15 -0300

    Clase usuario

---
+++ desafio-uno-clases/desafio-uno-clases/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_uno_clases
+{
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string nombreUsuario;
+        private string contrasenia;
+        private string mail;
+      
+    }
+}
