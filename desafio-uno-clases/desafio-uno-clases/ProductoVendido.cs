f787c46d305fb86c151f1c6911f4ed0fd54a41a8
rechegoy <rebeca.echegoy@gmail.com>
2022-11-02 11:41:06 -0300

    Producto vendido

---
+++ desafio-uno-clases/desafio-uno-clases/ProductoVendido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_uno_clases
+{
+    public class ProductoVendido
+    {
+        private int id;
+        private int idProducto;
+        private int stock;
+        private int idVenta;
+    }
+}
