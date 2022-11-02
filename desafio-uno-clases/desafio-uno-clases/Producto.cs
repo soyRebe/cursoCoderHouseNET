917283ebce1cbdfbe2fe0cc42653fa0012e210ab
rechegoy <rebeca.echegoy@gmail.com>
2022-11-02 11:40:47 -0300

    Clase producto

---
+++ desafio-uno-clases/desafio-uno-clases/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_uno_clases
+{
+    public class Producto
+    {
+        private int id;
+        private string descripcion;
+        private float costo;
+        private float precioVenta;
+        private int stock;
+        private int idUsuario;
+
+    }
+}
