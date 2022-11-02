e0b21e97780954584f020ccc84e23c5e01fb7de3
rechegoy <rebeca.echegoy@gmail.com>
2022-11-02 11:42:32 -0300

    Clase venta

---
+++ desafio-uno-clases/desafio-uno-clases/Venta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_uno_clases
+{
+   public class Venta
+    {
+        private int id;
+        private string comentarios;
+    }
+}
